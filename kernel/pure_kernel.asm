f42517e349e250381d68ec53e277f0d560dc7481
XCaminhante <caminhante@i2pmail.org>
2021-05-01 20:08:07 -0300

    O código nem estava compilando mais, tudo corrigido agora

--- kernel/pure_kernel.asm
+++ kernel/pure_kernel.asm
@@ -8,9 +8,7 @@ extrn linkd
 extrn linkb
 extrn _end
 
-include 'proc32.inc'
-
-VIDEO_VRAM equ 0xb8000
+TEXT_VGARAM equ 0xb8000
 
 BLACK equ 0
 BLUE equ 1
@@ -28,6 +26,16 @@ LIGHT_RED equ 12
 LIGHT_MAGENTA equ 13
 LIGHT_BROWN equ 14
 WHITE equ 15
+
+macro cor letra,fundo {
+  mov ah, letra + fundo*16
+}
+
+struc texto conteudo, letra, fundo {
+  @@: db conteudo
+  .tam = $-@b
+  .cor = letra + fundo*16
+}
 
 section '.text' executable
 start:
@@ -52,75 +60,106 @@ multiboot:
   dd start
   dd _end
 
-proc main
+main:
   mov esp, kernel_stack
   push 0
   popf
-  mov [term_color], LIGHT_GRAY+BLACK*16
 
   push ebx ;multiboot magic number
   push eax ;multiboot-info structure
 
-  stdcall clr
-  mov al, 'X'
-  stdcall putchar
+  call clr
+  mov bx, 1
+  mov dx, 1
+  mov al, t1.tam
+  mov ah, t1.cor
+  mov ecx, t1
+  call print_XY
+
+  add bx, t1.tam
+  mov al, t2.tam
+  mov ah, t2.cor
+  mov ecx, t2
+  call print_XY
+
+  mov bx, 1
+  inc dx
+  mov ah, t3.cor
+  mov al, t3.tam
+  mov ecx, t3
+  call print_XY
+
+  add bx, t3.tam
+  mov ah, t4.cor
+  mov al, t4.tam
+  mov ecx, t4
+  call print_XY
 
   .kernel_limbo:
   cli
   hlt
   jmp .kernel_limbo
-endp
+  ret
 
-proc clr
+; al: caractere
+; ah: cor
+; bx: coluna
+; dx: linha
+putchar_XY:
+  push eax ebx edx edi
+  cld
+  mov edi, TEXT_VGARAM
+  dec dx
+  dec bx
+  imul bx, 2
+  imul dx, 80*2
+  add bx, dx
+  add edi, ebx
+  stosw
+  pop edi edx ebx eax
+  ret
+
+; al: quantos caracteres (máximo 80)
+; ah: cor
+; bx: coluna inicial
+; ecx: ponteiro da string
+; dx: linha inicial
+print_XY:
+  push eax ebx ecx edx esi edi
+  mov esi, ecx
+  xor ecx, ecx
+  mov cl, al
+  cld
+  mov edi, TEXT_VGARAM
+  dec dx
+  dec bx
+  imul bx, 2
+  imul dx, 80*2
+  add bx, dx
+  add edi, ebx
+  .print_loop:
+    lodsb
+    stosw
+    loop .print_loop
+  pop edi esi edx ecx ebx eax
+  ret
+
+clr:
   push eax ecx edi
   cld
-  mov edi, VIDEO_VRAM
-  mov eax, 0x000f
+  mov edi, TEXT_VGARAM
+  mov al, ' '
+  mov ah, 0x0f
   mov ecx, 80*25
   rep stosw
   pop edx ecx eax
   ret
-endp
-
-proc putchar
-  push ebx ecx edx
-  cmp al, 10
-  je .newline
-  .prepare:
-  push eax
-  xor ebx, ebx
-  xor edx, edx
-  mov ecx, 25
-  xor eax, eax
-  .calcule_addr:
-  mov bl, [src_column]
-  mov al, [src_line]
-  mul ecx
-  add ebx, eax
-  .print:
-  pop eax
-  shl ax, 8
-  mov al, [term_color]
-  mov [VIDEO_VRAM+ebx], ax
-  cmp [src_column], 79
-  je .newline
-  inc [src_column]
-  jmp .end
-  .newline:
-  mov [src_column], 0
-  inc [src_line]
-  cmp [src_line], 25
-  jne .end
-  mov [src_line], 0
-  .end:
-  pop edx ecx ebx
-  ret
-endp
 
 section '.bss' writeable
-term_color rb 1
-src_line rb 1
-src_column rb 1
-
-rb 48*1024
+rb 1024
 kernel_stack:
+
+t1 texto "Hello, i'm your kernel ", WHITE,BLACK
+t2 texto "AND I AM FUCKING ALIVE!", LIGHT_RED,BLACK
+t3 texto "Now with colors! ", LIGHT_CYAN,BLACK
+t4 texto "A W E S O M E", LIGHT_GREEN,BLACK
